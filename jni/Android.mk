522f92a8031ac297f205bb56d85d6c8b16bf08fb
Alexander Björck <bjorck@ustwo.com>
2016-01-11 14:36:17 +0100

    mod so it default builds/runs for newer androids (only) -PIE

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+APP_PLATFORM := android-16
 
 LOCAL_MODULE := stress
 LOCAL_SRC_FILES := stress.c
-LOCAL_CFLAGS += -DPACKAGE=\"stress\" -DVERSION=\"1.0.4\"
+LOCAL_CFLAGS += -fPIE -DPACKAGE=\"stress\" -DVERSION=\"1.0.4\"
+LOCAL_LDFLAGS += -fPIE -pie
 
 include $(BUILD_EXECUTABLE)
